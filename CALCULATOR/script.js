1f0904bc079ae9d82e5a458417fab7cf5e030355
sandy75037 <147439925+sandy75037@users.noreply.github.com>
2024-10-15 18:27:13 +0530

    Add files via upload

---
+++ CALCULATOR/script.js
@@ -0,0 +1,8 @@
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
